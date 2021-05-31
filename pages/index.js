73e5b16c67c790a65d2c8433706d462272bc2686
lkmSasanga <lkmsasanga@gmail.com>
2021-05-31 13:39:50 +0530

    Link added to routing

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,20 @@
-// our-domain.com/
+import Link from "next/link";
+import { Fragment } from "react";
 
 function HomePage() {
-    return <h1>The Home Page</h1>
+  return (
+    <Fragment>
+      <h1>The Home Page</h1>
+      <ul>
+        <li>
+          <Link href="/news/nextjs-is-a-great-framework">
+            NextJS is a great Framework
+          </Link>
+        </li>
+        <li>Something Else</li>
+      </ul>
+    </Fragment>
+  );
 }
 
 export default HomePage;
